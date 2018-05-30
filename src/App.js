0941f66e0abaa966a735e3f5820f5c30cbc1ecdd
Semana SI <semanasi@FA7-L40M21.local>
2018-05-30 20:43:10 -0300

    Show Components

--- src/App.js
+++ src/App.js
@@ -1,63 +1,72 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import List from './components/List';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
 
-  handleChangeDescription = (value) => {
-    
-  }
+		}
+	}
 
-  submitList = () => {
-    let hora = new Date().getHours();
-    let min = new Date().getMinutes();
-    let idElemento = 0
-    idElemento = this.state.list.length === 0 ? 1 : this.state.list[this.state.list.length - 1].id + 1;
-    let obj = {
-      id: idElemento,
-      description: this.state.description,
-      horaAtual: hora + ":" + min,
-      feito: false
-    }
-    this.state.list.push(obj);
-    this.setState({ description: "" })
-  }
+	handleChangeDescription = (value) => {
 
-  removerTodaList = () => {
-    
-  }
+	}
 
-  deleteElement = (id) => {
-    // this.setState({ list: this.state.list.filter(e => e.id !== id) })
-  }
+	submitList = () => {
+		let hora = new Date().getHours();
+		let min = new Date().getMinutes();
+		let idElemento = 0
+		idElemento = this.state.list.length === 0 ? 1 : this.state.list[this.state.list.length - 1].id + 1;
+		let obj = {
+			id: idElemento,
+			description: this.state.description,
+			horaAtual: hora + ":" + min,
+			feito: false
+		}
+		this.state.list.push(obj);
+		this.setState({
+			description: ""
+		})
+	}
 
-  tarefaFeita = (element) => {
-    // this.setState({
-    //   list: this.state.list.filter(e => {
-    //     if (e === element) {
-    //       e.feito = true;
-    //     }
-    //     return true
-    //   })
-    // })
-  }
+	removerTodaList = () => {
 
-  tarefaNaoFeita = (element) => {
-     
-  }
+	}
 
-  render() {
-    return (
-      <div className="container">
-        <h1>React pra quem te quer :D</h1>
-      </div>
-    );
-  }
+	deleteElement = (id) => {
+		// this.setState({ list: this.state.list.filter(e => e.id !== id) })
+	}
+
+	tarefaFeita = (element) => {
+		// this.setState({
+		//   list: this.state.list.filter(e => {
+		//	 if (e === element) {
+		//	   e.feito = true;
+		//	 }
+		//	 return true
+		//   })
+		// })
+	}
+
+	tarefaNaoFeita = (element) => {
+
+	}
+
+	render() {
+		return (
+			<div className = "container" >
+				<Header / >
+				<br />
+				<Form / >
+				<br />
+				<List / >
+			</div>
+		);
+	}
 }
 
 export default App;
